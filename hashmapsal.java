2f892b112450f8996958c027bff27d48e19085fc
Sidharth12 <sidharthsadhwani3@gmail.com>
2016-11-25 15:30:45 +0530

    Add files via upload

---
+++ hashmapsal.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class hashmapsal{
+public static void main(string args[]){
+Scanner st=new Scanner(System.in);
+HashMap<String,Integer> hobj=new HashMap<String,Integer>();
+System.out.print("enter the no of employees : ");
+int a=st.nextInt();
+for(int i=0;i<a;i++){
+System.out.print("enter the details of cust of employees : ");
+System.out.print("enter the name of cust of employees : ");
+hobj.put(st.nextLine());
+System.out.print("enter the salary of cust of employees : ");
+hobj.put(st.nextInt();)
+}
+}
+}
